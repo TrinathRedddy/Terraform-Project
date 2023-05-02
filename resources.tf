37bb1143d29971b77f28f68b8f20ecd9cb862621
Trinath Reddy <64123592+TrinathRedddy@users.noreply.github.com>
2023-05-02 14:14:16 +0530

    Resources.tf

--- resources.tf
+++ resources.tf
@@ -1,8 +1,6 @@
 resource "aws_instance" "awscloud" {
     ami = "ami-0578f2b35d0328762"
     instance_type = "t2.micro"
-    key_name = "terraform-key"
-    vpc_security_group_ids = [aws_security_group.awscloudsg.id]
     tags = {
         Name = "awscloud"
     }
